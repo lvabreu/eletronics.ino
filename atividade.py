7232c900e4d721b43472bac9b403e8a406ec999d
Ana Lívia de Abreu Macêdo Santos <127103785+lvabreu@users.noreply.github.com>
2025-10-08 18:53:35 -0300

    class.py

--- atividade.py
+++ atividade.py
@@ -1,13 +1,14 @@
 class setor: #localizar no plano; largura e altura, perimetro e area 
-    
-    def __init__(self, x = 2.0, y=2.0):
+    public
+    def __init__(self, x , y):
         self.x = x
         self.y = y
-    def setPerimetro(self, x, y):
-
-
-    def setArea(self,x, y):
+    private 
+        def setPerimetro(lado, x, y):
+              perímetro = lado * x + lado * y 
+            return perímetro 
     
-    def
-
+        def setArea(x, y):
+            area = x * y 
+        return area
     
